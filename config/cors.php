6f666657f1ebc0546631b91baa0bbf665e2a8878
meko568 <mohammedelbardan82@gmail.com>
2026-04-25 20:07:46 +0300

    cros configuration

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://finance-tracking-frontend-yogf.vercel.app'],
+    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://finance-tracking-frontend-n9ed2s7tj-mohammeds-projects-dfbef21c.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
